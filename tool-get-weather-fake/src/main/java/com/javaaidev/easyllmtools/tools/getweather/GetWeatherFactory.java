77cfd1403d0ea2646bb5a883de92b10d0c40dd84
Fu Cheng <alexcheng1982@gmail.com>
2025-03-22 18:10:24 +1300

    update example

--- tool-get-weather-fake/src/main/java/com/javaaidev/easyllmtools/tools/getweather/GetWeatherFactory.java
+++ tool-get-weather-fake/src/main/java/com/javaaidev/easyllmtools/tools/getweather/GetWeatherFactory.java
@@ -1,12 +1,19 @@
 package com.javaaidev.easyllmtools.tools.getweather;
 
-import com.javaaidev.easyllmtools.llmtoolspec.ToolFactory;
+import com.javaaidev.easyllmtools.llmtoolspec.ConfigurableToolFactory;
 import com.javaaidev.easyllmtools.tools.getweather.model.GetWeatherConfiguration;
+import com.javaaidev.easyllmtools.tools.getweather.model.GetWeatherParameters;
+import com.javaaidev.easyllmtools.tools.getweather.model.GetWeatherReturnType;
 
-public class GetWeatherFactory implements ToolFactory<GetWeather, GetWeatherConfiguration> {
+public class GetWeatherFactory implements ConfigurableToolFactory<GetWeatherParameters, GetWeatherReturnType, GetWeather, GetWeatherConfiguration> {
 
     @Override
     public GetWeather create(final GetWeatherConfiguration config) {
         return new GetWeather(config);
+    }
+
+    @Override
+    public String toolId() {
+        return "GetWeather";
     }
 }
