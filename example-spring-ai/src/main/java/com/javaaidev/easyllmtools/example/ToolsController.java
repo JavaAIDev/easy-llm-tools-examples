e69fdfb04d0020dd828c07453eff889e61ea4a3e
Fu Cheng <alexcheng1982@gmail.com>
2025-04-10 00:08:05 +1200

    update ui

--- example-spring-ai/src/main/java/com/javaaidev/easyllmtools/example/ToolsController.java
+++ example-spring-ai/src/main/java/com/javaaidev/easyllmtools/example/ToolsController.java
@@ -1,10 +1,16 @@
 package com.javaaidev.easyllmtools.example;
 
+import com.javaaidev.chatagent.model.ChatAgentRequest;
+import com.javaaidev.chatagent.model.ChatAgentResponse;
+import com.javaaidev.chatagent.springai.ModelAdapter;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ToolsController {
@@ -38,12 +44,13 @@ public class ToolsController {
   }
 
   @PostMapping("/chat")
-  public ChatOutput chat(@RequestBody ChatInput chatInput) {
-    var content = chatClient.prompt()
-        .user(chatInput.input())
+  public Flux<ServerSentEvent<ChatAgentResponse>> chat(@RequestBody ChatAgentRequest request) {
+    var messages = ModelAdapter.fromRequest(request);
+    return ModelAdapter.toStreamingResponse(chatClient.prompt()
+        .messages(messages.toArray(new Message[0]))
         .functions("sectionFormatGet")
-        .call().content();
-    return new ChatOutput(content);
+        .stream().chatResponse()
+    );
   }
 
   public record ChatInput(String input) {
